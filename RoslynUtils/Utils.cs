77acbce256ea3ecca07bb6ead9e690822408f0be
Benj <Benjamin.Schwerdtner@gmail.com>
2020-05-13 09:38:45 +0200

    Add roslyn utils

---
+++ RoslynUtils/Utils.cs
@@ -0,0 +1,22 @@
+using Microsoft.CodeAnalysis;
+
+namespace RoslynUtils {
+    public static class Utils {
+        public static bool EqualToAny(this ITypeSymbol that, params ITypeSymbol[] others) {
+            foreach (var typeSymbol in others) {
+                if (that.EqualTo(typeSymbol)) return true;
+            }
+
+            return false;
+
+        }
+        public static bool EqualTo(this ITypeSymbol that, ITypeSymbol other) {
+            if(ReferenceEquals(that,other)) return true;
+            if(ReferenceEquals(that,null)) return false;
+            if(ReferenceEquals(other,null)) return false;
+            if(that.Equals(other)) return true;
+            return that.MetadataName == other.MetadataName;
+        }
+    }
+
+}
